b1a29e4776682238666b9852fabd6bc0354bbdc9
BarbaraSalla <barbara.msalla@gmail.com>
2023-01-25 05:15:02 -0300

    adding new information

--- JQuery/Fundamentals/script.js
+++ JQuery/Fundamentals/script.js
@@ -63,3 +63,54 @@ $(function(){
 $(function(){
     $('#teste').find('#objetoDentroTeste').addClass('estiloBackground');
 });
+
+/*######################## NAVEGANDO ENTRE ELEMENTOS ###################*/
+//Selecionar elemento anterior
+$('.botao').parent()    //Seleciona o elemento que vem antes no caso: <ul></ul>
+$('.botao').parent().parent()   //Retorna: <div></div>
+
+//Busca e seleciona elemento específico
+$('#divCorpo').find('.botao')   //Retorna :
+                                //<li class="botao">Item 1</li>
+                                //<li class="botao">Item 2</li>
+                                //<li class="botao">Item 3</li>
+                                //<li class="botao">Item 4</li>
+$('#divCorpo').find('.botao').eq(0)    //Retorna primeiro elemento: <li class="botao">Item 1</li>
+
+//Buscar o elemento mais próximo dentro da estrutura que se relaciona. Se tiver fora do escopo nãõ encontra
+$('.botao').closest("#divCorpo")   //Retorna: <div id="divCorpo"></div>
+
+//Buscar elemento fora da estrutura que engloba. É preciso sair para o elemento que se realciona com o outro elemento:
+$('.botao').closest("#divCorpo").find("#divQualquer")   //Retorna: <div id="divQualquer"></div>
+
+//DICA: Não usar parent, pois o código pode ser alterado e não ser mais o elemento anterior, preferir find()
+
+/*######################## CSS, Styling e Dimensões ###################*/
+
+//Adiciona ao elemento uma classe style que foi construida pelo usuário
+$('h1').addClass("fundovermelho") 
+
+//Remove classe do elemento
+$('h1').removeClass("fundovermelho")
+
+//Verificar se o elemento possui a classe específica Ex.: if($('h1').hasClass("fundovermelho")){};
+$('h1').hasClass("fundovermelho")
+
+//Adicionar atributo CSS específico em um elemento html passando 2 parametros
+$('h1').css("color", "#000FFF");
+
+//Adicionando vários atributos num elemento html, caso precise de muitos elementos é mais ideal que construa uma classe style e aplique ao elemento como a classe "fundovermelho"
+$('h1').css("font-size", "16px").css("background-color", "#FF0000");    
+
+/*######################## Guardando dados em um elemento ###################*/
+
+//O JQuery permite adicionar qualquer propriedade que o usuário especificar, no caso do exemplo, foi add uma propriedade 'data-idade' com valor de '90'. Mas esses dados são gravados no HTML
+$('input').attr('data-idade', '90');    // Cenário: <input type="text" name="nome"> 
+                                        //Resultado: <input type="text" name="nome" data-idade="90">
+
+//O Jquery , no entanto, permite adicionar propriedades ao proprio jquery (Memória do computador)
+$('input').data('idade', '90'); // Cenário: <input type="text" name="nome"> 
+                                //Resultado: <input type="text" name="nome">
+                                //Ao buscar pelo parametro no JQuery: $('input').data('idade'); Resp-> 90
+
+/*########################  ###################*/
